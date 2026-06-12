e5b780739d33ed56c53deeef5e701b13de3fcc1b
dashyk0 <dasapusina111@gmail.com>
2026-06-13 00:16:38 +0300

    Change MYSQL_DATABASE default to 'railway'

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ class Config:
     MYSQL_PORT = int(os.environ.get('MYSQLPORT', 3306))
     MYSQL_USER = os.environ.get('MYSQLUSER', 'root')
     MYSQL_PASSWORD = os.environ.get('MYSQLPASSWORD', 'root123*')
-    MYSQL_DATABASE = os.environ.get('MYSQLDATABASE', 'library_db')
+    #MYSQL_DATABASE = os.environ.get('MYSQLDATABASE', 'library_db')
+    MYSQL_DATABASE = os.environ.get('MYSQLDATABASE', 'railway')
     UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static', 'uploads')
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
